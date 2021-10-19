01c6757b0dc7d24723237fa17e6e62ceeef33cd1
Дмитрий Бодунов <nedochai@yandex.ru>
2021-10-19 15:40:54 +0300

    3 задание, начало

--- 3home/3 homework.py
+++ 3home/3 homework.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QComboBox, QPushButton, QHBoxLayout, QVBoxLayout, \
-    QSizePolicy
+    QSizePolicy, QLCDNumber
 
 
 class ConvertWidget(QWidget):
@@ -9,19 +9,54 @@ class ConvertWidget(QWidget):
         super(ConvertWidget, self).__init__()
         self.setWindowTitle('Миникалькулятор')
 
+        self.main_layout = QHBoxLayout(self)
+
+        self.input_layout = QVBoxLayout(self)
+        self.output_layout = QVBoxLayout(self)
+
         self.input_value_up = QLineEdit(self)
+        self.input_value_up.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         self.input_value_down = QLineEdit(self)
+        self.input_value_down.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.output_value_sum = QLineEdit(self)
+        self.output_value_sum = QLCDNumber(self)
+        self.output_value_sum.setDigitCount(5)
+        self.output_value_sum.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.output_value_razn = QLineEdit(self)
+        self.output_value_razn = QLCDNumber(self)
+        self.output_value_razn.setDigitCount(5)
+        self.output_value_razn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.output_value_proizv = QLineEdit(self)
+        self.output_value_proizv = QLCDNumber(self)
+        self.output_value_proizv.setDigitCount(5)
+        self.output_value_proizv.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.output_value_chast = QLineEdit(self)
+        self.output_value_chast = QLCDNumber(self)
+        self.output_value_chast.setDigitCount(5)
+        self.output_value_chast.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.button = QPushButton
+        self.button = QPushButton(self)
+        self.button.clicked.connect(self.calk)
+        self.button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.input_layout.addWidget(self.input_value_up)
+        self.input_layout.addWidget(self.input_value_down)
+
+        self.output_layout.addWidget(self.output_value_sum)
+        self.output_layout.addWidget(self.output_value_chast)
+        self.output_layout.addWidget(self.output_value_proizv)
+        self.output_layout.addWidget(self.output_value_razn)
+
+        self.main_layout.addLayout(self.input_layout)
+        self.main_layout.addWidget(self.button)
+        self.main_layout.addLayout(self.output_layout)
+        self.setLayout(self.main_layout)
+
+    def calk(self):
+        first = self.input_value_up.text()
+        second = self.input_value_down.text()
+        self.output_value_sum.setText(str(first + second))
 
 
 if __name__ == '__main__':
