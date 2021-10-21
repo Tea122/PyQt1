0746c4d4aba2baf5b9cf6e903d81a6839f21900b
Дмитрий Бодунов <nedochai@yandex.ru>
2021-10-21 23:20:17 +0300

    Pattern

---
+++ Pattern.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QComboBox, QPushButton, QHBoxLayout, QVBoxLayout, \
+    QSizePolicy, QLCDNumber, QLabel
+
+
+class ConvertWidget(QWidget):
+    def __init__(self):
+        super(ConvertWidget, self).__init__()
+        self.setWindowTitle('Миникалькулятор')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = ConvertWidget()
+    wnd.show()
+    sys.exit(app.exec())
