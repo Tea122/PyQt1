dbf4a2d75d4544db91a9f5216f6456c9bf3bb50a
Дмитрий Бодунов <nedochai@yandex.ru>
2021-10-21 23:12:03 +0300

    Задание 3 сделано!

--- 3home/3 homework not Layout.py
+++ 3home/3 homework not Layout.py
@@ -7,11 +7,11 @@ class ConvertWidget(QWidget):
     def __init__(self):
         super(ConvertWidget, self).__init__()
         self.setWindowTitle('Миникалькулятор')
-        self.setFixedSize(500, 150)
+        self.setFixedSize(500, 130)
 
-        # ------------------------Размер и положение готово------------------------------------
+        # ------------------------Размер и положение готово----------------------------
         self.input_value_up = QLineEdit(self)
-        self.input_value_up.resize(100, 22)
+        self.input_value_up.resize(100, 25)
         self.input_value_up.move(50, 30)
 
         self.input_value_up_t = QLabel('Первое число(целое):', self)
@@ -32,38 +32,55 @@ class ConvertWidget(QWidget):
         self.button.clicked.connect(self.calk)
         self.button.resize(75, 22)
         self.button.move(175, 50)
-        # ------------------------------
+        # ------------------------Размер и положение готов-----------------------------
         self.output_value_summ = QLCDNumber(self)
-        self.output_value_summ.setDigitCount(5)
-        self.output_value_summ.resize(100, 20)
-        self.output_value_summ.move(370, 25)
+        self.output_value_summ.resize(100, 25)
+        self.output_value_summ.move(370, 15)
 
-        self.input_value_summ_t = QLabel('Сумма:', self)
-        self.input_value_summ_t.setBuddy(self.output_value_summ)
-        self.input_value_summ_t.move(320, 30)
+        self.output_value_summ_t = QLabel('Сумма:', self)
+        self.output_value_summ_t.setBuddy(self.output_value_summ)
+        self.output_value_summ_t.move(320, 20)
 
-        # self.phoneEdit = QLineEdit(self)
-        # self.phoneLabel = QLabel("&Phone:", self)
-        # self.phoneLabel.setBuddy(self.phoneEdit)
+        # ------------------Размер и положение готов-----------------------------------
 
-        # -------
+        self.output_value_razn = QLCDNumber(self)
+        self.output_value_razn.resize(100, 25)
+        self.output_value_razn.move(370, 41)
 
-        # self.output_value_razn = QLCDNumber(self)
-        # self.output_value_razn.setDigitCount(5)
-        # self.output_value_razn.resize(150, 20)
-        # self.output_value_razn.move(120, 100)
+        self.output_value_razn_t = QLabel('Разность:', self)
+        self.output_value_razn_t.setBuddy(self.output_value_razn)
+        self.output_value_razn_t.move(305, 45)
 
-        # self.output_value_proizv = QLCDNumber(self)
-        # self.output_value_proizv.setDigitCount(5)
-        # self.output_value_proizv.resize(140, 20)
-        # self.output_value_proizv.move(140, 100)
+        # --------------------------Размер и положение готов---------------------------
 
-    # ------------------------------------------------------------
+        self.output_value_proizv = QLCDNumber(self)
+        self.output_value_proizv.resize(100, 25)
+        self.output_value_proizv.move(370, 67)
+
+        self.output_value_proizv_t = QLabel('Произведение:', self)
+        self.output_value_proizv_t.setBuddy(self.output_value_razn)
+        self.output_value_proizv_t.move(278, 72)
+
+        # ---------------------------Размер и положение готов---------------------------------
+
+        self.output_value_chast = QLCDNumber(self)
+        self.output_value_chast.resize(100, 25)
+        self.output_value_chast.move(370, 93)
+
+        self.output_value_chast_t = QLabel('Частное:', self)
+        self.output_value_chast_t.move(309, 99)
 
     def calk(self):
         first = self.input_value_up.text()
         second = self.input_value_down.text()
-        self.output_value_sum.setText(str(first + second))
+        # print(type(first), type(second))
+        self.output_value_summ.display(int(first) + int(second))
+        self.output_value_razn.display(int(first) - int(second))
+        self.output_value_proizv.display(int(first) * int(second))
+        if int(second) == 0:
+            self.output_value_chast.display('Error')
+        else:
+            self.output_value_chast.display(int(first) / int(second))
 
 
 if __name__ == '__main__':
